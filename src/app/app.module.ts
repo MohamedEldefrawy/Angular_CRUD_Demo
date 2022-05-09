f33da87b2b9faba9a915b9f4f02b1edba69925a8
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-09 12:29:52 +0200

    Create file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StudentsModule} from "./students/students.module";
+import {DepartmentsModule} from "./departments/departments.module";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StudentsModule,
+    DepartmentsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
